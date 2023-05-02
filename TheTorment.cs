429613e8cdb6ff816eb72cad0408dec2593a6b00
Zerokk <mandrygin.2013@mail.ru>
2023-05-02 15:00:45 +0300

    Initial commit

---
+++ TheTorment.cs
@@ -0,0 +1,18 @@
+using System;
+
+using Terraria.ModLoader;
+
+namespace TheTorment;
+
+public class TheTorment : Mod {
+    public const string PATHTO_GRAPHICSFOLDER = $"{nameof(TheTorment)}/Source/Graphics";
+
+    internal static TheTorment? _instance;
+
+    public static TheTorment Instance
+        => _instance ?? throw new InvalidOperationException("An instance of the mod has not yet been created.");
+
+    internal TheTorment() {
+        _instance = this;
+    }
+}
